1966316ae464c8874fc3ec0f89d819002a17224c
Lorellana <lorellana.ordonez@gmail.com>
2022-05-30 18:28:09 +0200

    test(cypress): add first test in cypress/integration

---
+++ cypress/integration/app.spec.tsx
@@ -0,0 +1,4 @@
+it("should work", () => {
+    cy.visit("http:localhost:3001");
+    cy.get("p").should("have.text", "Hola");
+});
--- cypress/integration/first.tsx
+++
@@ -1,5 +0,0 @@
-/// <reference types="Cypress" />
-it('simple', () => {
-    cy.visit('https://www.google.es');
-    cy.get('.gLFyf').type('Cypress');
-    })
